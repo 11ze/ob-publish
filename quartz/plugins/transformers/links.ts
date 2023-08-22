786b6dc870774f48ea97fe327f249998e5f309c0
11ze <wangzecn@qq.com>
2023-08-23 02:33:49 +0800

    fix: 修复中文反向链接失效的问题

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -64,7 +64,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options> | undefined> =
                   const canonicalDest = url.pathname
                   const [destCanonical, _destAnchor] = splitAnchor(canonicalDest)
                   const simple = simplifySlug(destCanonical as FullSlug)
-                  outgoing.add(decodeURIComponent(simple))
+                  outgoing.add(decodeURIComponent(simple) as SimpleSlug)
                 }
 
                 // rewrite link internals if prettylinks is on
