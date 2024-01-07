85b21712234b4c2afe7a0e4b4c1d765f482c33e9
11ze <11ze@users.noreply.github.com>
2024-01-07 09:31:27 +0000

    deploy: 29970b860f892195f3c497e400229ab889b41bec

---
+++ modify_updated.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 说明：
+
+# 请在 crontab 中配置定时任务，例如每分钟执行一次
+#   * * * * * cd "dirname" && ./modify_updated.sh
+
+current_time=$(date +"%Y-%m-%dT%H:%M:%S+08:00")
+
+# 获取 git status 的输出
+git_status=$(git status)
+
+# 如果暂存区有 md 文件发生了更改，获取并打印所有更改的 md 文件的路径
+if echo "$git_status" | grep -q ".md"; then
+
+  # 循环打印每个文件名，可以另外处理每个文件名
+  echo "$git_status" | grep -A1000 "Changes not staged for commit:" | grep ".md" | while read -r line; do
+  filePath=$(echo "$line")
+
+  filename=$(echo "$filePath" | awk -F': ' '{print $2}') # modified: xxx.md
+  if [ -z "$filename" ]; then
+    filename=$(echo "$filePath" )
+  fi
+
+  # 移除首位空格
+  filename=$(echo "$filename" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  echo "filename: $filename"
+
+  # 检查每个文件的前 10 行是否包含 "updated:" 字段
+  if head -10 "$filename" | grep -q "updated:"; then
+      # 使用 sed 命令将 "updated:" 之后的内容替换为当前时间
+      echo "updated: $current_time $filename" >> auto_updated.log
+      sed -i -e "1,10s/\(updated: \).*/\1$current_time/" "$filename"
+
+      # 删除 sed 命令生成的备份文件
+      if [ -f "$filename-e" ]; then
+        rm "$filename-e"
+        git add "$filename"
+      fi
+
+  fi
+
+  done
+fi
