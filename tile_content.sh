a3bc29a4eebc627fce15b22f2577cce81c1ff7df
11ze <wangzecn@qq.com>
2024-04-24 15:01:56 +0800

    chore: 添加新命令处理笔记

---
+++ tile_content.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 检查有 Git 才执行
+if which git > /dev/null; then
+    git submodule update --init --recursive
+fi
+
+rm -rf content/.obsidian
+mv content/*.md content/Atlas
+find content/ -name "*.md" | xargs -I file  mv -f file content
