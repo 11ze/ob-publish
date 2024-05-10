9971c37ac735ada38d80e5e024099466c5455bba
11ze <wangzecn@qq.com>
2024-05-10 12:18:58 +0800

    chore: remove 🫧

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🫧 11ze",
+    pageTitle: "11ze",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
